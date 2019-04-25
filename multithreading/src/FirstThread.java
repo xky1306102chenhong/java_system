fa0626c025b93eb8716aafc62b439305be396e20
xky1306102chenhong <1358434388@qq.com>
2019-04-25 16:30:20 +0800

    new

---
+++ multithreading/src/FirstThread.java
@@ -0,0 +1,25 @@
+
+/**
+ * @author Chris Chen
+ * @date 2019/4/25 下午3:54
+ */
+public class FirstThread extends Thread{
+    private int i;
+
+    @Override
+    public void run(){
+        for (; i< 100; i++){
+            System.out.println(getName() + " " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i=0; i<100; i++){
+            System.out.println(Thread.currentThread() + " " + i);
+            if (i == 20 ){
+                new FirstThread().start();
+                new FirstThread().start();
+            }
+        }
+    }
+}
